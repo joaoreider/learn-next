eac507356ad04d7cd6b36275554c394bd8079009
Joao Paulo <joaopauloj405@gmail.com>
2024-01-25 09:51:37 -0300

    Add Monaco Editor and Snippet Edit Form components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>
+    <Link
+      key={snippet.id}
+      className="flex justify-between items p-2 border rounded"
+      href={`/snippets/${snippet.id}`}
+    >
       <h3>{snippet.title}</h3>
-      <p>{snippet.code}</p>
-    </div>
+      <div>View</div>
+    </Link>
   ));
 
   return (
     <div>
-      <h1>Home</h1>
-      {renderedSnippets}
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link
+          className="border p-2 rounded text-white bg-blue-500"
+          href="/snippets/new"
+        >
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col  gap-2">{renderedSnippets}</div>
     </div>
   );
 }
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 interface SnippetShowPageProps {
   params: {
@@ -7,6 +8,7 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const snippet = await db.snippet.findFirst({
     where: {
       id: parseInt(props.params.id),
@@ -17,8 +19,21 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   }
   return (
     <div>
-      <h1>Snippet</h1>
-      <h3>{snippet.title}</h3>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
     </div>
   );
 }
